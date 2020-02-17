e6ba81b1dd720aa86c1e6d27ed76d0979edb7c9e
asus <asus@LAPTOP-SUB6TP6L>
2020-02-17 13:48:45 +0800

    项目的第一天

---
+++ src/test/java/com/situ/scrm/ScrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.situ.scrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
