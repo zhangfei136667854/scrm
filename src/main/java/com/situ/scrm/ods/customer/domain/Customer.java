ea8a6fd972a551c87614d57abe6fa9329de9ddf2
asus <asus@LAPTOP-SUB6TP6L>
2020-02-26 10:44:19 +0800

    20200225

---
+++ src/main/java/com/situ/scrm/ods/customer/domain/Customer.java
@@ -0,0 +1,14 @@
+package com.situ.scrm.ods.customer.domain;
+
+import java.io.Serializable;
+
+import com.situ.scrm.commons.domain.BaseClass;
+
+public class Customer extends BaseClass implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
